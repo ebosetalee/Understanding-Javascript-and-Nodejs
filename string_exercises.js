390ad6fe2b9f2414d641406b7b8510e389d364ea
ebosetalee <princess01ella.miley@gmail.com>
2021-01-07 09:19:10 +0100

    initial commit

---
+++ string_exercises.js
@@ -0,0 +1,66 @@
+// Write a function firstChar, which returns the first character that is not a space when a string is passed.
+function firstChar(string){
+    let result = string.trim()
+    return result.charAt(0)
+}
+console.log(firstChar(" Thomas Kings "))
+
+
+// Write a function indexOfIgnoreCase taking two strings and determining the first occurrence of the second 
+// string in the first string. The function should be case insensitive.
+function indexOfIgnoreCase(text1, text2){
+    text1 = text1.toLowerCase()
+    text2 = text2.toLowerCase()
+    var result = text1.indexOf(text2);
+    return result
+}
+
+
+// Write a function secondIndexOf, taking two strings and determining the second occurrence of the 
+// second string in the first string. If the search string does not occur twice, -1 should be returned.
+function secondIndexOf2(string1, string2){
+    return string1.lastIndexOf(string2);
+}
+// This is wrong if the string2 is bit, it will return 10, instead of -1.
+function secondIndexOf(string1, string2){
+    var answer = string1.indexOf(string2);
+    if (answer == -1){
+        return answer
+    } else if (answer != -1)
+        new_string = string1.slice(answer+1);
+        answer2 = new_string.indexOf(string2);
+        if (answer2 == -1){
+            return(answer2)
+        } else{
+            return answer2 + answer +1
+        }
+}
+console.log(secondIndexOf("White Rabbit", "it"))
+
+
+// Write a function firstWord, taking a string and returning the first word in that string. 
+// The first word are all characters up to the first space.
+function firstWord(text){
+    var i = 0;
+    var first_word = "";
+    while (text[i]){
+        if (text[i]== " "){
+            break;
+        } else {
+            first_word += text[i];
+        }
+        i++
+    }
+    return first_word
+}
+
+console.log(firstWord("see and stop"))
+
+
+// Write a function normalize, that replaces '-' with '/' in a date string.
+// Example: normalize('20-05-2017') should return '20/05/2017'.
+function normalize(text){
+    var result = text.replace(/-/g, "/");
+    return result
+}
+console.log(normalize("20-05-2017"))
---
+++ strings.js
@@ -0,0 +1,115 @@
+var b = [5, 6, 9, 8, 3, 2]
+console.log(b.length)
+
+// .toUpperCase(); .toLowerCase();
+var a = "mathHna"
+console.log(a.toUpperCase())
+
+console.log(a.toLowerCase())
+
+function toCase(string){
+    var result = string.toLowerCase() + "-" + string.toUpperCase()
+    return result
+}
+console.log(toCase("shine"))
+
+// How to escape a character, use \
+var characts = "It\'s alright."
+console.log(characts)
+characts = "We are the so-called \"Vikings\" from the north.";
+console.log(characts)
+// other uses of backslash is:
+// \b	Backspace
+// \f	Form Feed
+// \n	New Line
+// \r	Carriage Return
+// \t	Horizontal Tabulator
+// \v	Vertical Tabulator
+
+// using index to get a character
+//charAt(index)
+let char0 = "Frants".charAt(0)
+console.log(char0)
+// It is also the same as 
+let char = "Frants"
+console.log(char[0])
+// Write a function shortcut that takes two strings and returns the initial letters of theses strings.
+function shortcut(string1, string2){
+    return string1[0] + string2.charAt(0);
+}
+console.log(shortcut("Amnesty", "International"))
+
+// Also, There's charCodeAt() method returns the unicode of the character at a specified index in a string:
+// The method returns a UTF-16 code (an integer between 0 and 65535).
+char = "HELLO WORLD";
+console.log(char.charCodeAt(0)); // returns 72
+
+
+// trim() method removes whitespaces from both ends of a string.
+char = ' Rosa Parks ';
+console.log(char.trim());
+
+
+// replace() method replaces a specified value with another value in a string:
+var strings = "Please visit Microsoft and Microsoft!";
+console.log(strings.replace("Microsoft", "W3Schools"));
+// Note that by default, the replace() method replaces only the first match
+// Also note that it is case sensitive. Thus, better to use a regular expression as seen below:
+// to use regex with an i flag (insensitive):
+console.log(strings.replace(/MICROSOFT/i, "W3Schools"));
+// Note that Note that regular expressions are written without quotes.
+// to use regex for all matches not just first match:
+console.log(strings.replace(/Microsoft/g, "W3Schools"));
+
+
+// The indexOf method also allows you to specify the position from which the search should start.
+// This is done with a second parameter.
+let n1 = 'White Rabbit'.indexOf('it', 3);
+console.log(n1)
+
+n1 = 'White Rabbit'.indexOf('it', 1);
+console.log(n1)
+
+
+// The search() method searches a string for a specified value and returns the position of the match.
+var string_list = "Please locate where 'locate' occurs!";
+console.log(string_list.search("locate"));
+// it returns 7, because its the 7th word using space as a word
+
+// Note that the search() and indexOf() methods aren't equal:
+// The search() method cannot take a second start position parameter.
+// The indexOf() method cannot take powerful search values (regular expressions).
+
+
+// Extracting String Parts
+// There are 3 methods for extracting a part of a string:
+
+// slice(start, end)
+// substring(start, end)
+// substr(start, length)
+// The slice() extracts a part of a string and returns the extracted part in a new string.
+// The method takes 2 parameters: the start position, and the end position (end not included),
+console.log(string_list.slice(10));
+
+var res = string_list.slice(-12);
+console.log(res)
+
+// The substring() is similar to slice().
+// The difference is that substring() cannot accept negative indexes
+res = string_list.substring(7, 13);
+console.log(res)
+
+// substr() is similar to slice().
+// The difference is that the second parameter specifies the length of the extracted part
+// Also, If the second parameter is not set, all the characters from start position to the 
+// end of the string are extracted.
+let see = 'see and stop'.substr(0, 3);
+let and = 'see and stop'.substr(4, 3);
+let stopp = 'see and stop'.substr(8);
+console.log(see, and, stopp)
+
+
+// The concat() method joins two or more strings:
+var text1 = "Hello";
+var text2 = "World";
+console.log(text1.concat(" ", text2));
