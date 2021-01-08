eb3168ce7e95864f3e287cfc86e2d8f85208715f
ebosetalee <princess01ella.miley@gmail.com>
2021-01-08 19:10:05 +0100

    name and random fixed

--- love_calculator.js
+++ love_calculator.js
@@ -1,39 +1,39 @@
-const males = ["timi", "funmi", "jasmine", "praise", "anna", "lisa", "bola"]
-const females = ["bisi", "seun", "joseph", "frank", "daniel", "bayo", "david"]
+const males = ["timi", "funmi", "jasmine", "praise", "anna", "lisa", "bola"];
+const females = ["bisi", "seun", "joseph", "frank", "daniel", "bayo", "david"];
 
 
 function calculator(male, female){
-    const calculator_name = male + "loves" + female
+    const calculator_name = male + "loves" + female;
     console.log(calculator_name);
 
-    let love_algorithm = ""
+    let love_algorithm = "";
 
     for (let index = 0; index < calculator_name.length; index++){
-        let count = 0
-        const letter = calculator_name[index]
+        let count = 0;
+        const letter = calculator_name[index];
         for (x of calculator_name){
             if (x == letter){
-                count+= 1
+                count+= 1;
             }
         }
-        love_algorithm += count
+        love_algorithm += count;
     }
-    console.log(love_algorithm)
+    console.log(love_algorithm);
 
     while (love_algorithm.length > 2){
-        let new_love_algorithm = ""
-        index = 0;
-        let index2 = love_algorithm.length - 1 
+        let new_love_algorithm = "";
+        count_from_front = 0;
+        let count_from_back = love_algorithm.length - 1 ;
         const fixed_length = love_algorithm.length / 2;
-        while (index < fixed_length){
-            if (index != index2){
-                const result = parseInt(love_algorithm[index]) + parseInt(love_algorithm[index2]);
+        while (count_from_front < fixed_length){
+            if (count_from_front != count_from_back){
+                const result = parseInt(love_algorithm[count_from_front]) + parseInt(love_algorithm[count_from_back]);
                 new_love_algorithm += result;
             }else {
-                new_love_algorithm += love_algorithm[index];
+                new_love_algorithm += love_algorithm[count_from_front];
             }
-            index++
-            index2 --
+            count_from_front++
+            count_from_back--
         }
         love_algorithm = new_love_algorithm
         console.log(love_algorithm)
